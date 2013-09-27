e8fed8625e88f99423157e1a0bad4e72d83d30f9
Kevin Reintjes <kreintjes@gmail.com>
2013-09-27 15:55:55 +0200

    XSS module: made a begin with the sanitizers tests.

--- xss/app/controllers/application_controller.rb
+++ xss/app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery
   respond_to :html
+
+  before_filter :parse_method
+
+  RUN_MODE = nil # Let the system decide based on the environment
+
+  BASE_TESTS_ENABLED = true
+
+  def running?
+    return RUN_MODE if RUN_MODE.present?
+    Rails.env.production?
+  end
+  helper_method :running?
+
+  # In URLs we replace ? in the method with a -, so the scanners will not mess with it (because they think it is part of the GET query string).
+  def parse_method
+    params[:method].gsub!('-', '?') if params[:method].present?
+  end
 end
--- xss/app/controllers/base_test_controller.rb
+++ xss/app/controllers/base_test_controller.rb
@@ -1,7 +1,33 @@
 class BaseTestController < ApplicationController
+  include ERB::Util
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::SanitizeHelper
+
   def sanitizers_form
   end
 
   def sanitizers_perform
+    case params[:method]
+    when 'html_escape', 'escape_once', 'strip_tags'
+      @result = self.send(params[:method], params[:input])
+    when 'sanitize'
+      case params[:option]
+      when 'none'
+        options = { :tags => [], :attributes => [] }
+      when 'no_tags'
+        options = { :tags => [] }
+      when 'no_attributes'
+        options = { :attributes => [] }
+      when 'custom'
+        options = { :tags => %w(table tr td b i u strong em p div span ul li), :attributes => %w(id class title) }
+      when 'default'
+        options = {}
+      end
+      @result = self.send(params[:method], params[:input], options)
+    when 'automatic'
+      @result = params[:input]
+    else
+      @result = params[:input].html_safe # So we will see if we forget to implement a method
+    end
   end
 end
